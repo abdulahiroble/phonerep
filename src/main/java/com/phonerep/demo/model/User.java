26ee246d1339546ff8800f66bf46d7b3d35766c6
Thomas WEst <thom8750@stud.kea.dk>
2021-05-21 14:20:15 +0200

    spring security tilføjet, samt admin login

---
+++ src/main/java/com/phonerep/demo/model/User.java
@@ -0,0 +1,89 @@
+package com.phonerep.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userid;
+
+
+
+    private String username;
+    private String password;
+    private boolean active;
+    private String roles;
+
+
+
+
+    public User (String username, String password, Boolean active, String roles) {
+
+        this.username = username;
+        this.password = password;
+        this.active = active;
+        this.roles = roles;
+
+    }
+
+
+
+    public User () {
+
+    }
+
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userid);
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+}
---
+++ src/main/java/com/phonerep/demo/service/MyUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.phonerep.demo.service;
+
+import com.phonerep.demo.model.MyUserDetails;
+import com.phonerep.demo.model.User;
+import com.phonerep.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + username));
+
+        return user.map(MyUserDetails::new).get();
+    }
+}
