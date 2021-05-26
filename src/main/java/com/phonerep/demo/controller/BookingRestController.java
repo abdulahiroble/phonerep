f00d0c28712f152ae412bb984ca1bb2d317f5703
abdulahi roble <abdulahi_2610@hotmail.com>
2021-05-26 16:57:38 +0200

    Add ressources to security

--- src/main/java/com/phonerep/demo/controller/BookingRestController.java
+++ src/main/java/com/phonerep/demo/controller/BookingRestController.java
@@ -1,15 +1,24 @@
 package com.phonerep.demo.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 import com.phonerep.demo.model.Booking;
 import com.phonerep.demo.model.Service;
 import com.phonerep.demo.model.Shop;
 import com.phonerep.demo.repositories.BookingRepository;
 import com.phonerep.demo.repositories.ServiceRepsitory;
 import com.phonerep.demo.repositories.ShopRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(value = "*")
@@ -34,8 +43,6 @@ public class BookingRestController {
         int x = Integer.parseInt(shopx.getShopname());
 
         shopx.setShopid(x);
-
-
 
         return bookingRepository.save(booking);
     }
